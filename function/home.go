c4b31587f6e7322fb1ae96daf0019999f8ed55be
CalvoTom <tom.calvo@ynov.com>
2023-12-15 15:03:43 +0100

    Input V.1

---
+++ function/home.go
@@ -0,0 +1,17 @@
+package hangmanweb
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	hangman "github.com/CalvoTom/HangmanPackage"
+)
+
+func Home(w http.ResponseWriter, r *http.Request, H *hangman.HangManData) {
+	template, err := template.ParseFiles("./index.html", "./templates/wordToFind.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, H)
+}
--- server.go
+++ server.go
@@ -1,39 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	hangmanweb "hangmanweb/function"
 	"net/http"
-	"text/template"
+
+	hangman "github.com/CalvoTom/HangmanPackage"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	template, err := template.ParseFiles("./index.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	template.Execute(w, nil)
-}
-
-func formulaire(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		if err := r.ParseForm(); err != nil {
-			log.Fatal(err)
-		}
-
-		input := r.FormValue("input")
-		log.Println(input)
-		http.Redirect(w, r, "http://localhost:8080", http.StatusFound)
-
-	default:
-		fmt.Fprintf(w, "Only POST")
-	}
-}
-
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/hangman", formulaire)
+	var H *hangman.HangManData
+	H = hangman.InitialiseStruc("words.txt")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hangmanweb.Home(w, r, H)
+	})
+	http.HandleFunc("/hangman", func(w http.ResponseWriter, r *http.Request) {
+		hangmanweb.Formulaire(w, r, H)
+	})
+
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.ListenAndServe(":8080", nil)
